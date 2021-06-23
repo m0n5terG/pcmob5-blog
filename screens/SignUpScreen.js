0313585379b649738139d7898f30455512ea527e
m0n5terG <m0n5terpaerai@gmail.com>
2021-06-23 13:18:40 +0800

    setup signup

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,13 +1,151 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { commonStyles } from "../styles/commonStyles";
+import { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ActivityIndicator,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+
+const API = "http://m0n5terg.pythonanywhere.com";
+const API_SIGNUP = "/newuser";
 
 export default function SignUpScreen({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorText, setErrorText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [refresh, setRefresh] = useState(false);
+  const [error, setError] = useState(false);
+
+  async function signup () {
+    console.log("---- Signing Up ----");
+    Keyboard.dismiss();
+
+    try {
+      setLoading(true);
+      const response = await axios.post(API + API_SIGNUP, {
+        username,
+        password,
+      });
+      console.log("Success signing up!");
+      console.log(response);
+      navigation.navigate("Account");
+
+    } catch (error) {
+      setLoading(false);
+      console.log("Error signing up!");
+      console.log(error.response);  
+      setErrorText(error.response.data.description);
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const token = await AsyncStorage.getItem("token");
+      console.log(token);
+      if (token == null) {
+        setError(true);
+        setUsername(null);
+      } else {
+        try {
+          const response = await axios.get(API + API_WHOAMI, {
+            headers: { Authorization: `JWT ${token}` },
+          });
+          setUsername(response.data.username);
+          setLoading(false);
+        } catch (error) {
+          setError(true);
+          setUsername(null);
+          setLoading(false);
+        }
+      }
+    })();
+    setRefresh(false);
+  }, [refresh]);
+
   return (
-    <View style={commonStyles.container}>
-      <Text>Sign Up Screen</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Sign Up</Text>
+        <Text style={styles.fieldTitle}>Username</Text>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={username}
+          onChangeText={(input) => setUsername(input)}
+        />
+        <Text style={styles.fieldTitle}>Password</Text>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          autoCompleteType="password"
+          autoCorrect={false}
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(input) => setPassword(input)}
+        />
+        <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity onPress={signup} style={styles.loginButton}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+        {loading ? (
+          <ActivityIndicator style={{ marginLeft: 20, marginBottom: 20 }} />
+        ) : null}
+        </View>
+        <Text style={styles.errorText}>{errorText}</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 24,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  fieldTitle: {
+    fontSize: 18,
+    marginBottom: 12,
+  },
+  input: {
+    borderColor: "#999",
+    borderWidth: 1,
+    marginBottom: 24,
+    padding: 4,
+    height: 36,
+    fontSize: 18,
+    backgroundColor: "white",
+  },
+  loginButton: {
+    backgroundColor: "blue",
+    width: 120,
+    alignItems: "center",
+    padding: 18,
+    marginTop: 12,
+    marginBottom: 36,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  errorText: {
+    color: "red",
+    height: 40,
+  },
+});
